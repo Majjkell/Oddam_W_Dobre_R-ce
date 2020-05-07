78143b8b0c8ecf942237e5504c006bcb89227f27
Mikołaj Żeberkiewicz <mikolajzeberkiewicz@gmail.com>
2020-05-07 17:22:49 +0200

    Instytution entity and repo

---
+++ src/main/java/pl/coderslab/charity/model/Instytution.java
@@ -0,0 +1,15 @@
+package pl.coderslab.charity.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Instytution {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+}
