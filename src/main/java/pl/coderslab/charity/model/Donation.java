10276c3cee49d32dfaa4beae43a3641773f09e58
Mikołaj Żeberkiewicz <mikolajzeberkiewicz@gmail.com>
2020-05-11 15:09:18 +0200

    Getters Setters Constructor with Lombok

--- src/main/java/pl/coderslab/charity/model/Donation.java
+++ src/main/java/pl/coderslab/charity/model/Donation.java
@@ -1,10 +1,19 @@
 package pl.coderslab.charity.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Donation {
     @Id
