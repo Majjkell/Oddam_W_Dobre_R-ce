134d918ee9290fd2a13d067af79675e1f5d34173
Majjkell <mikolajzeberkiewicz@gmail.com>
2020-06-02 19:20:29 +0200

    User model and lombok for role/user

--- src/main/java/pl/coderslab/charity/model/Role.java
+++ src/main/java/pl/coderslab/charity/model/Role.java
@@ -1,6 +1,7 @@
 package pl.coderslab.charity.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
@@ -10,6 +11,7 @@ import javax.persistence.Id;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 public class Role {
     @Id
---
+++ src/main/java/pl/coderslab/charity/model/User.java
@@ -0,0 +1,29 @@
+package pl.coderslab.charity.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true, length = 60)
+    private String username;
+    private String password;
+    private int enabled;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+}
+
+
