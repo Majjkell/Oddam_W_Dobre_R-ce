663eff72e4dcf3726e95f5200c5126b62d7bf640
Majjkell <mikolajzeberkiewicz@gmail.com>
2020-06-02 19:15:09 +0200

    Role for Spring Secruity

--- src/main/java/pl/coderslab/charity/SecurityConfig.java
+++ src/main/java/pl/coderslab/charity/SecurityConfig.java
@@ -1,10 +1,36 @@
 package pl.coderslab.charity;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("{noop}user123").roles("USER")
+                .and()
+                .withUser("admin1").password("{noop}admin123").roles("ADMIN");
+    }
+
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/form").authenticated()
+                .and().formLogin();
+    }
 }
